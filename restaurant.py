96e111d9e5f3743d23f74ee7250bdc0275dd3d69
martin <martin.drucks@auticon>
2020-09-01 13:58:24 +0200

    first commit restaurant kata

---
+++ restaurant.py
@@ -0,0 +1,57 @@
+
+
+# 11:10 01.09 kata zwischendurch = restaurant.py
+# https://www.codewars.com/kata/5d23d89906f92a00267bb83d?utm_source=newsletter&utm_medium=email&utm_campaign=weekly_coding_challenges_sponsored_by_scout_apm&utm_term=2020-08-25
+
+# 6 kyu
+# New Cashier Does Not Know About Space or Shift
+# Some new cashiers started to work at your restaurant.
+# They are good at taking orders, but they don't know how to capitalize words, or use a space bar!
+# All the orders they create look something like this:
+# "milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza"
+# The kitchen staff are threatening to quit, because of how difficult it is to read the orders.
+# Their preference is to get the orders as a nice clean string with spaces and capitals like so:
+# "Burger Fries Chicken Pizza Pizza Pizza Sandwich Milkshake Milkshake Coke"
+# The kitchen staff expect the items to be in the same order as they appear in the menu.
+# The menu items are fairly simple, there is no overlap in the names of the items:
+
+# 1. Burger
+# 2. Fries
+# 3. Chicken
+# 4. Pizza
+# 5. Sandwich
+# 6. Onionrings
+# 7. Milkshake
+# 8. Coke
+
+def restaurant(order): # order = String, --> order als Liste 
+	
+	# result = str('ert').title();
+	s = 'myorder'
+	result = s.title()
+	print (result)
+	# result = title(order)
+	return result
+	
+restaurant('')
+
+Robert
+github
+martinberlin2
+pw 21
+
+vcs version control system 
+
+auch shell zum git üben
+
+	
+# Hi Robert, 	
+	# ich mach grade eine Kata, es sollen Wörter Gross geschrieben werden...und...
+	# s = 'mystring'
+	# result = s.title()
+	
+	# ergibt bei mir 
+	# name 'title' is not defined!
+	# und kein stackoverflow sagt, dass ich irgendwas importieren muss.
+	# es sieht sooo einfach aus....
+	# was kann das sein?
---
+++ test_restaurant.py
@@ -0,0 +1,20 @@
+﻿
+
+# 11:10 01.09 kata zwischendurch = restaurant.py
+# 
+# "milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza"
+
+import NaTALi
+evalTC = NaTALi.evalTC
+execFuncWithExc = NaTALi.execFuncWithExc
+execMethWithExc = NaTALi.execMethWithExc
+
+import restaurant
+restaurant = restaurant.restaurant
+
+import logging
+	
+def test_restaurant(): 
+	logging.info(" ----- Start test_restaurant -----")
+	evalTC(1, "KataOrder", execFuncWithExc(restaurant, ['milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza']), "Burger Fries Chicken Pizza Pizza Pizza Sandwich Milkshake Milkshake Coke")
+	logging.info(" ----- End test_restaurant -----")
